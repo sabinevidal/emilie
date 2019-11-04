cae841ea0cd712324dfe4400d7f2e883753d39fc
Sabine Vidal <svidal@live.co.za>
2019-11-04 16:41:59 +0200

    Updated site-branding and header

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 					) );
 					?>
 				</nav><!-- #site-navigation -->	
-			
+			<div class="branding-header">
 				<div class="site-branding">
 				
 					<?php
@@ -57,13 +57,14 @@
 					
 				</div><!-- .site-branding -->
 
-				
-				<?php if ( get_header_image() ) : ?>
-					<div id="site-header">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-							<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-						</a>				</div>
-				<?php endif; ?>
+					<?php if ( get_header_image() ) : ?>
+						<div id="site-header">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+								<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+							</a>				
+						</div>
+					<?php endif; ?>
+				</div>
 		</div>
 	</header><!-- #masthead -->
 
