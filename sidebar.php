753695323c4796e354905db163dedfb61351e737
Sabine Vidal <svidal@live.co.za>
2019-11-08 13:36:19 +0200

    Updated search bar

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area">
+    <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+            <div><label class="screen-reader-text" for="s">Search for:</label>
+            <div class="search">
+                <input type="search" class="search-field" placeholder="Search" name="s" id="s" />
+                    <!-- <input type="submit" id="searchsubmit" value="Search" /> -->
+                    <button type="submit" class="searchButton" id="searchsubmit"><i class="fas fa-search"></i></button>
+            </div>
+        </div>
+    </form>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
