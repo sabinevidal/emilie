8987fa8cc4b5351c51eaf6f08c2dfa1854cbe5e7
Sabine Vidal <svidal@live.co.za>
2019-11-15 23:41:08 +0200

    Trying to add gallery to alt front page

--- frontx-page.php
+++ frontx-page.php
@@ -26,91 +26,16 @@ get_header();
     
         <section class="portfolio-work">
 			<div class="homepage-portfolio-work">
-				<?php query_posts('posts_per_page=3&post_type=portfolio_projects'); ?>
-				<?php while ( have_posts() ) : the_post(); 
-                    $image_1 = get_field("image_1");
-                    $image_1 = get_field("image_2");
-                    $image_1 = get_field("image_3");
-                    $image_1 = get_field("image_4");
-                    $image_1 = get_field("image_5");
-                    $image_1 = get_field("image_6");
-                    $image_1 = get_field("image_7");
-                    $image_1 = get_field("image_8");
-                    $image_1 = get_field("image_9");
-                    $link = get_field('link');
-                    $size = "full";
-                    
-				?>
+				<?php while ( have_posts() ) : the_post(); ?>
 				<div class="project-images">
                         <div class="container">
                             <div class="card-columns">
-                                <?php if ($image_1 = get_field('image_1')) { ?>
                                     <div class="card project-info">
-                                    <a href="<?php get_field('link') ?>" target="_blank"><?php if($image_1) {
-                                                echo wp_get_attachment_image( $image_1, $size );
-                                        } ?> 
-                                    </a>
+                                     <?php echo do_shortcode('[gallery option1="value1"]'); ?>   
                                     </div>
-                                <?php } ?>
-                                <?php if ($image_2 = get_field('image_2')) { ?>
-                                    <div class="card">
-                                        <?php if($image_2) {
-                                                echo wp_get_attachment_image( $image_2, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_3 = get_field('image_3')) { ?>
-                                    <div class="card">
-                                        <?php if($image_3) {
-                                                echo wp_get_attachment_image( $image_3, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_4 = get_field('image_4')) { ?>
-                                    <div class="card">
-                                        <?php if($image_4) {
-                                                echo wp_get_attachment_image( $image_4, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_5 = get_field('image_5')) { ?>
-                                    <div class="card">
-                                        <?php if($image_5) {
-                                                echo wp_get_attachment_image( $image_5, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_6 = get_field('image_6')) { ?>
-                                    <div class="card">
-                                        <?php if($image_6) {
-                                                echo wp_get_attachment_image( $image_6, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_7 = get_field('image_7')) { ?>
-                                    <div class="card">
-                                        <?php if($image_7) {
-                                                echo wp_get_attachment_image( $image_7, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_8 = get_field('image_8')) { ?>
-                                    <div class="card">
-                                        <?php if($image_8) {
-                                                echo wp_get_attachment_image( $image_8, $size );
-                                        } ?> 
-                                    </div>
-                                <?php } ?>
-                                <?php if ($image_9 = get_field('image_9')) { ?>
-                                    <div class="card">
-                                        <?php if($image_9) { 
-                                                echo wp_get_attachment_image( $image_9, $size );
-                                        } ?>
-                                    </div>
-                                <?php } ?>
                             </div>
                         </div>
-                    </div>
+                </div>
 
 				<?php endwhile; ?>
 					<?php wp_reset_query(); ?>  
